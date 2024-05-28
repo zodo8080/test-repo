d687ce29eb312d63ccfa26598365472a1ad5e5be
Ramana N <ramana.nagisetty@gmail.com>
2024-05-27 20:11:51 -0400

    first commit

---
+++ lambda_function.py
@@ -0,0 +1,12 @@
+import uuid
+
+def lambda_handler(event, context):
+    # Generate a UUID
+    unique_id = str(uuid.uuid4())
+    
+    # Your code logic here
+    
+    return {
+        'statusCode': 200,
+        'body': unique_id
+    }
